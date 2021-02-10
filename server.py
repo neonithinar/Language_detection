60f4ab4ee2737a64f876ae7fa8c0a7789c6864a0
neonithinar <neonithinar@gmail.com>
2021-02-10 15:34:38 +0530

    modified server.py to handle unknown languages

--- server.py
+++ server.py
@@ -11,18 +11,6 @@ with open('vectorizer.pkl', 'rb') as f:
     vectorizer_loaded = pickle.load(f)
 
 
-def preprocess_input(text):
-    data = text
-    data = data.lower()
-    data = data.replace(r'[^\w\s]+', '')
-    data = [data]
-
-    X = vectorizer_loaded.fit_transform(data)
-    feature_names = vectorizer_loaded.get_feature_names()
-    predict_features = pd.DataFrame(data=X.toarray(), columns=feature_names)
-    return predict_features
-
-
 predicted_lang = ["arabic", "czech", "danish", "finnish", "lithuanian", "macedonian",
                   "dutch", "polish", "serbian", "swedish"]
 
@@ -39,23 +27,28 @@ def home():
 def predict():
     # render the results from HTML GUI
     # Get the data from the POST request.
-    data =request.form.values()
-    print(data, "jhosi")
-    # data = data.lower()
-    # data = data.replace(r'[^\w\s]+', '')
-    # data = [data]
-    #
-    # X = vectorizer_loaded.fit_transform(data)
-    # feature_names = vectorizer_loaded.get_feature_names()
-    # X_pred = pd.DataFrame(data=X.toarray(), columns=feature_names)
-    #
-    # # if
-    #
-    # # Make prediction using model loaded from disk as per the data.
-    # var = np.argmax(model_prep.predict(X_pred), axis=-1).item()  # store the array of prediction into a variable
+    data =request.form["input_text"]
 
-    # return render_template('index.html', prediction_text = "The input language is {}".format(predicted_lang[var]))
-    return render_template('index.html', prediction_text = "The input language is {}".format(data))
+    data = data.lower()
+    data = data.replace(r'[^\w\s]+', '')
+    data = [data]
+
+    X = vectorizer_loaded.fit_transform(data)
+    feature_names = vectorizer_loaded.get_feature_names()
+    X_pred = pd.DataFrame(data=X.toarray(), columns=feature_names)
+
+    predictions = model_prep.predict(X_pred)
+    print(max(predictions[0]))
+    if max(predictions[0]) < 0.65:
+        return render_template('index.html', prediction_text = " I'm sorry, I do not know this language yet :(")
+
+    else:
+
+        # Make prediction using model loaded from disk as per the data.
+        var = np.argmax(model_prep.predict(X_pred), axis=-1).item()  # store the array of prediction into a variable
+
+        return render_template('index.html', prediction_text = "The input language is {}".format(predicted_lang[var]))
+        # return render_template('index.html', prediction_text = "The input language is {}".format(data))
 
 if __name__ == '__main__':
     app.run(port=5000, debug = True)
